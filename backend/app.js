0da362f1271ed51a2f733d040c2d23e76d3b52e3
Abhishek Maurya <abhishekmaurya.official@gmail.com>
2025-01-30 02:56:02 +0530

    swagger docs setup

--- backend/app.js
+++ backend/app.js
@@ -42,8 +42,12 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/carImage", carImageRoutes);
 app.use("/api/v1/cars", carRoutes);
 
+const swaggerUICss =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css";
 const swaggerOptions = {
-  url: "/docs/swagger.yaml",
+  customCss:
+    ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
+  customCssUrl: swaggerUICss,
 };
 
 const swaggerDocument = YAML.load(yamlFilePath);
