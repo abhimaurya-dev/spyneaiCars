63a39924a094783b7a35369899b2866d4dca28bb
Abhishek Maurya <abhishekmaurya.official@gmail.com>
2025-01-30 02:10:11 +0530

    added swagger.yaml docs

--- backend/routes/carImageRoutes.js
+++ backend/routes/carImageRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { uploadProductImageController } from "../controllers/productImageControllers/uploadProductImageController.js";
+import { isAuthenticated } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/upload", uploadProductImageController);
+router.post("/upload", isAuthenticated, uploadProductImageController);
 
 export default router;
